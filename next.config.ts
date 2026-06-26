e91d3e2015e6740c5b099c4c9e2d5bc3bfc58612
Jagdish Mahanta <43695894+JmJagdish@users.noreply.github.com>
2026-06-26 18:01:26 +0530

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/jmjagdish.github.io",
+  assetPrefix: "/jmjagdish.github.io/",
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ["192.168.*.*"],
 };
 
